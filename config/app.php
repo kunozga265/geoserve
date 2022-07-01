1efac0b9f05f91742e09a282ba61ae54d3e1487d
kunozga265 <kunozgamlowoka@gmail.com>
2022-07-01 09:58:43 +0200

    Converted some information values to numbers

--- config/app.php
+++ config/app.php
@@ -221,7 +221,7 @@ return [
         'RateLimiter' => Illuminate\Support\Facades\RateLimiter::class,
         'Redirect' => Illuminate\Support\Facades\Redirect::class,
         // 'Redis' => Illuminate\Support\Facades\Redis::class,
-        'RequestForm' => Illuminate\Support\Facades\Request::class,
+        'Request' => Illuminate\Support\Facades\Request::class,
         'Response' => Illuminate\Support\Facades\Response::class,
         'Route' => Illuminate\Support\Facades\Route::class,
         'Schema' => Illuminate\Support\Facades\Schema::class,
