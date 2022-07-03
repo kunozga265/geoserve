4a7e98704b4c9a70a871a4d03fab133c3efd133d
kunozga265 <kunozgamlowoka@gmail.com>
2022-07-03 14:02:02 +0200

    Applying roles

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ use Inertia\Inertia;
     ]);
 });*/
 
-Route::group(['middleware'=>['auth:sanctum', 'verified']],function (){
+Route::group(['middleware'=>['auth:sanctum', 'verified','roles']],function (){
 
     Route::get('/', [
         "uses"  => "App\Http\Controllers\RequestFormController@dashboard",
@@ -32,36 +32,44 @@ Route::group(['middleware'=>['auth:sanctum', 'verified']],function (){
 
     Route::get('/index', [
         "uses"  => "App\Http\Controllers\RequestFormController@index",
+        'roles' =>['employee','management']
     ])->name('index');
 
     Route::get('/approved', [
         "uses"  => "App\Http\Controllers\RequestFormController@approved",
+        'roles' =>['employee','management']
     ])->name('approved');
 
     Route::get('/finance', [
         "uses"  => "App\Http\Controllers\RequestFormController@finance",
+        'roles' =>['employee','management']
     ])->name('finance');
 
 
     Route::group(['prefix'=>'users'],function() {
         Route::get('/', [
             "uses" => "App\Http\Controllers\UserController@index",
+            'roles' =>['employee','management']
         ])->name('users');
 
         Route::get('/view/{id}', [
             "uses" => "App\Http\Controllers\UserController@show",
+            'roles' =>['employee','management']
         ])->name('users.show');
 
         Route::post('/verify/{id}', [
             "uses" => "App\Http\Controllers\UserController@verify",
+            'roles' =>['management']
         ])->name('users.verify');
 
         Route::post('/disable/{id}', [
             "uses" => "App\Http\Controllers\UserController@disable",
+            'roles' =>['management']
         ])->name('users.disable');
 
         Route::post('/discard/{id}', [
             "uses" => "App\Http\Controllers\UserController@discard",
+            'roles' =>['management']
         ])->name('users.discard');
 
     });
@@ -69,68 +77,84 @@ Route::group(['middleware'=>['auth:sanctum', 'verified']],function (){
     Route::group(['prefix'=>'projects'],function(){
         Route::get('/', [
             "uses"  => "App\Http\Controllers\ProjectController@index",
+            'roles' => ['employee','management']
         ])->name('projects');
 
         Route::get('/view/{id}', [
             "uses"  => "App\Http\Controllers\ProjectController@show",
+            'roles' => ['employee','management']
         ])->name('projects.show');
 
         Route::get('/create', [
             "uses"  => "App\Http\Controllers\ProjectController@create",
+            'roles' =>['administrator']
         ])->name('projects.create');
 
         Route::post('/store', [
             "uses"  => "App\Http\Controllers\ProjectController@store",
+            'roles' =>['administrator']
         ])->name('projects.store');
 
         Route::get('/edit/{id}', [
             "uses"  => "App\Http\Controllers\ProjectController@edit",
+            'roles' =>['administrator']
         ])->name('projects.edit');
 
         Route::post('/edit/{id}', [
             "uses"  => "App\Http\Controllers\ProjectController@update",
+            'roles' =>['administrator']
         ])->name('projects.update');
 
         Route::post('/verify/{id}', [
             "uses"  => "App\Http\Controllers\ProjectController@verify",
+            'roles' =>['management']
         ])->name('projects.verify');
 
         Route::delete('/delete/{id}', [
             "uses"  => "App\Http\Controllers\ProjectController@destroy",
+            'roles' => ['administrator','management']
         ])->name('projects.delete');
     });
 
     Route::group(['prefix'=>'vehicles'],function(){
         Route::get('/', [
             "uses"  => "App\Http\Controllers\VehicleController@index",
+            'roles' => ['employee','management']
         ])->name('vehicles');
 
         Route::get('/view/{id}', [
             "uses"  => "App\Http\Controllers\VehicleController@show",
+            'roles' => ['employee','management']
         ])->name('vehicles.show');
 
         Route::get('/create', [
             "uses"  => "App\Http\Controllers\VehicleController@create",
+            'roles' =>['administrator']
         ])->name('vehicles.create');
 
         Route::post('/store', [
             "uses"  => "App\Http\Controllers\VehicleController@store",
+            'roles' =>['administrator']
         ])->name('vehicles.store');
 
         Route::get('/edit/{id}', [
             "uses"  => "App\Http\Controllers\VehicleController@edit",
+            'roles' =>['administrator']
         ])->name('vehicles.edit');
 
         Route::post('/edit/{id}', [
             "uses"  => "App\Http\Controllers\VehicleController@update",
+            'roles' =>['administrator']
         ])->name('vehicles.update');
 
         Route::post('/verify/{id}', [
             "uses"  => "App\Http\Controllers\VehicleController@verify",
+            'roles' =>['management']
         ])->name('vehicles.verify');
 
         Route::delete('/delete/{id}', [
             "uses"  => "App\Http\Controllers\VehicleController@destroy",
+            'roles' => ['administrator','management']
         ])->name('vehicles.delete');
     });
 
@@ -138,46 +162,57 @@ Route::group(['middleware'=>['auth:sanctum', 'verified']],function (){
 
         Route::get('/create', [
             "uses"  => "App\Http\Controllers\RequestFormController@create",
+            'roles' =>['employee','management']
         ])->name('request-forms.create');
 
         Route::post('/store', [
             "uses"  => "App\Http\Controllers\RequestFormController@store",
+            'roles' =>['employee','management']
         ])->name('request-forms.store');
 
         Route::get('/view/{id}', [
             "uses"  => "App\Http\Controllers\RequestFormController@show",
+            'roles' =>['employee','management']
         ])->name('request-forms.show');
 
         Route::get('/edit/{id}', [
             "uses"  => "App\Http\Controllers\RequestFormController@edit",
+            'roles' =>['employee','management']
         ])->name('request-forms.edit');
 
         Route::post('/edit/{id}', [
             "uses"  => "App\Http\Controllers\RequestFormController@update",
+            'roles' =>['employee','management']
         ])->name('request-forms.update');
 
         Route::post('/approve/{id}', [
             "uses"  => "App\Http\Controllers\RequestFormController@approve",
+            'roles' =>['employee','management']
         ])->name('request-forms.approve');
 
         Route::post('/deny/{id}', [
             "uses"  => "App\Http\Controllers\RequestFormController@deny",
+            'roles' =>['employee','management']
         ])->name('request-forms.deny');
 
         Route::delete('/delete/{id}', [
             "uses"  => "App\Http\Controllers\RequestFormController@destroy",
+            'roles' =>['employee','management']
         ])->name('request-forms.delete');
 
         Route::delete('/discard/{id}', [
             "uses"  => "App\Http\Controllers\RequestFormController@discard",
+            'roles' =>['employee','management']
         ])->name('request-forms.discard');
 
         Route::post('/initiate/{id}', [
             "uses"  => "App\Http\Controllers\RequestFormController@initiate",
+             'roles' =>['accountant']
         ])->name('request-forms.initiate');
 
         Route::post('/reconcile/{id}', [
             "uses"  => "App\Http\Controllers\RequestFormController@reconcile",
+             'roles' =>['accountant']
         ])->name('request-forms.reconcile');
 
     });
