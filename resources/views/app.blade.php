d54fe9fbfa9e4ccd5cf1fa1d2696e1db0eb44822
kunozga265 <kunozgamlowoka@gmail.com>
2023-04-13 12:34:02 +0200

    Added favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,7 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+        <title>{{ config('app.name', 'Geoserve Engineering Group') }}</title>
+
+        <!-- Favicon -->
+        <link rel="icon" type="image/x-icon" href="{{asset('/favicon.png')}}">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Lato:400,700%7CRoboto:400,500%7CExo+2:600&display=swap" rel="stylesheet">
